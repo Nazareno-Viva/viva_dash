376d15861cd3276298cb3d623d21268ff822d0b8
Nazareno-Viva <vivalideres@gmail.com>
2025-05-07 11:43:26 -0300

    Add files via upload

---
+++ Igreja_do_Nazareno_VIVA.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Igreja do Nazareno VIVA", layout="wide")
+st.title("Igreja do Nazareno VIVA")
+
+st.write("Use a navegação para explorar as diferentes páginas.")
+
+
+
